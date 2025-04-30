ade6973bd0dacb4afcbfb709a5360e75fe50cf0b
user <user@XEDICEZEYNALOVA>
2025-04-30 22:13:44 +0400

    Add project files.

---
+++ ConsoleApp39/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp39
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime DateOfTheBirth = new DateTime(2007, 07, 07);
+             TimeSpan ts = DateTime.Now - DateOfTheBirth;
+             Console.WriteLine(ts.TotalDays);
+        }
+    }
+}
